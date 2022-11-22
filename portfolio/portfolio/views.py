ce4cef5f35cf21b9cbc425b4c045eb0ce7048d2c
SergeiGe <115823112+SergeiGe@users.noreply.github.com>
2022-11-22 20:09:38 +0300

    Джанго проект

---
+++ portfolio/portfolio/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+    #render - связывеет запросы, страницы и переменные
+    #в render можно отправить третий аргумент - это контекст (context)
+    #context указывается в виде словаря, где ключ - это имя переменной в html,
+    # а значение ключа - это значение этой переменной
+def reverse(request):
+    user_text = request.GET['username']
+    reversed_text = user_text[::-1]
+    return render(request,
+                  'reverse.html',
+                  {'word': reversed_text}
+                  )
